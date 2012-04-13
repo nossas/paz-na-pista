86532a546a797af848415680cbd069d768df3485
Nícolas Iensen <nicolas@quavio.com.br>
2012-04-13 18:50:09 -0300

    #1 GA

--- app/assets/javascripts/subscribers.js
+++ app/assets/javascripts/subscribers.js
@@ -7,3 +7,15 @@
   js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=413692758643806";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-26278513-3']);
+_gaq.push(['_setDomainName', 'paznapista.com.br']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
